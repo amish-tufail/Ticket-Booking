f20a957664f13a34364b9e2016cb0ed517eb817e
Amish Tufail <amish.tufail2002@gmail.com>
2023-01-24 20:49:28 +0500

    - Initial Project Setup - Add Data - Add Assets

---
+++ Ticket Booking/App/Ticket_BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ticket_BookingApp.swift
+//  Ticket Booking
+//
+//  Created by Amish Tufail on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Ticket_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
